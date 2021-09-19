dc8aabe097a9a4d50c27e1078a560c8a37d18145
Laura <laurass1004@gmail.com>
2021-09-19 17:41:44 -0500

    feat(app): update text principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'yogiweb';
+  
+  hiddenMenu=true;
+  
+  showMenu(){
+    this.hiddenMenu =!this.hiddenMenu;
+  }
 }
+
